c0895bace31367881138963d81374493133b358f
LvKvA <larskoets@hotmail.com>
2019-10-29 11:47:00 +0100

    Fix merge issues

--- src/flyscene.cpp
+++ src/flyscene.cpp
@@ -215,37 +215,20 @@ void Flyscene::raytraceScene(int width, int height) {
 	pixel_data.clear();
 }
 
-	for (int i = 0; i < lights.size(); ++i) {
-		//Each iterations shoots a ray to light
-		for (int i = 0; i < totalRaysShot; i++)
-		{
+Eigen::Vector3f  Flyscene::traceRay(Eigen::Vector3f& origin,
+	Eigen::Vector3f& dest) {
 
-			//Create two random floats between -0.075 and 0.075
-			//The range will be hardcoded unless we find a way to get the radius of the light
-			float randX = (rand() % 16) / 100 - 0.075;
-			float randY = (rand() % 16) / 100 - 0.075;
-			float randZ = (rand() % 16) / 100 - 0.075;
-
-			Eigen::Vector3f offset = Eigen::Vector3f(randX, randY, randZ);
-
-			//Shoot a ray from hit point to light center shifted by the offset vector
-			inters_point rayToLight = intersection(intersectionstruc.point, offset + lights[i]);
-
-			//See if ray reaches light. Increment counter if it does
-			if (!rayToLight.intersected)
-			{
-				raysReachLight++;
-			}
-		}
-	}
+	inters_point intersectionstruc = intersection(origin, dest);
+	float shadowratio = shadowRatio(intersectionstruc.point);
 
 	if (intersectionstruc.intersected == true) {
 		//Multiply the rgb value of the pixel by the shadow ratio
-		return (raysReachLight / (totalRaysShot * lights.size())) * shade(0, MAX_REFLECT, intersectionstruc.point, intersectionstruc.point - origin, intersectionstruc.face);
+		return shadowratio * shade(0, MAX_REFLECT, intersectionstruc.point, intersectionstruc.point - origin, intersectionstruc.face);
 	}
 	//if miss then return background color
 	return Eigen::Vector3f(backgroundColor.x(), backgroundColor.y(), backgroundColor.z());
 }
+
 
 //Calculates the direction of the refraction when the ray is inside the object and outside.
 Eigen::Vector3f Flyscene::refractionV(Eigen::Vector3f& view, Eigen::Vector3f& normal, float& index) {
@@ -448,25 +431,27 @@ float Flyscene::shadowRatio(Eigen::Vector3f intersectionP) {
 	//Counter for how many rays reach the light
 	int raysReachLight = 0;
 
-	//Each iterations shoots a ray to light
-	for (int i = 0; i < totalRaysShot; i++)
-	{
-		//Create two random floats between -0.075 and 0.075
-		//The range will be hardcoded unless we find a way to get the radius of the light
-		float randX = (rand() % 16) / 100 - 0.075;
-		float randY = (rand() % 16) / 100 - 0.075;
-		float randZ = (rand() % 16) / 100 - 0.075;
-
-		Eigen::Vector3f offset = Eigen::Vector3f(randX, randY, randZ);
-
-		//Shoot a ray from hit point to light center shifted by the offset vector
-		inters_point rayToLight = intersection(intersectionP, offset + lights.back());
-
-		//See if ray reaches light. Increment counter if it does
-		if (!rayToLight.intersected)
+	for (int i = 0; i < lights.size(); ++i) {
+		//Each iterations shoots a ray to light
+		for (int j = 0; j < totalRaysShot; ++j)
 		{
-			raysReachLight++;
+			//Create two random floats between -0.075 and 0.075
+			//The range will be hardcoded unless we find a way to get the radius of the light
+			float randX = (rand() % 16) / 100 - 0.075;
+			float randY = (rand() % 16) / 100 - 0.075;
+			float randZ = (rand() % 16) / 100 - 0.075;
+
+			Eigen::Vector3f offset = Eigen::Vector3f(randX, randY, randZ);
+
+			//Shoot a ray from hit point to light center shifted by the offset vector
+			inters_point rayToLight = intersection(intersectionP, offset + lights[i]);
+
+			//See if ray reaches light. Increment counter if it does
+			if (!rayToLight.intersected)
+			{
+				raysReachLight++;
+			}
 		}
 	}
-	return raysReachLight / totalRaysShot;
+	return raysReachLight / (totalRaysShot * lights.size());
 }
